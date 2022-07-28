955d65d86e9c6e586519b67101881b3dc756ac3b
AndreySmekhunov <smekhunov@gmail.com>
2022-07-28 16:10:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
-
+    let result = '';
+    let letter = '';
+    let letterBin = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        letterBin = expr.slice(i, i + 10);
+        letter = '';
+        for (let j = 0; j < 10; j += 2) {
+            if (letterBin[j] == '1' && letterBin[j+1] == '0') letter += '.';
+            if (letterBin[j] == '1' && letterBin[j+1] == '1') letter += '-'; 
+        }
+        if (letterBin === '**********') result += ' ' 
+            else result += MORSE_TABLE[letter];
+    
+    }    
+    return result;
+    }
+    
 module.exports = {
     decode
 }
